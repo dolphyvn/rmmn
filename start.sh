ff961fcd4452055b6580d390f9b29d34a41202eb
Anh Khoa Phan Le <dolphy.phanle@gmail.com>
2021-05-22 22:07:54 +0100

    add new miner

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ cd /usr/src/
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 sudo wget https://trex-miner.com/download/t-rex-0.20.3-linux.tar.gz
 
-tar xzvf lolMiner_v1.29_Lin64.tar.gz
-tar xzvf t-rex-0.20.3-linux.tar.gz
+sudo tar xzvf lolMiner_v1.29_Lin64.tar.gz
+sudo tar xzvf t-rex-0.20.3-linux.tar.gz
  
